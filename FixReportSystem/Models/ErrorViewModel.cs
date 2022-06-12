533458c13d7e7f8a4c9fce3ea172ac76a8ec5545
Loon-7w7 <99504287+Loon-7w7@users.noreply.github.com>
2022-06-12 11:53:12 -0500

    Agregar archivos de proyecto.

---
+++ FixReportSystem/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FixReportSystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
