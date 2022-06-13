5d2831325b4d20bcc7777da24467e3efdcb67677
Loon-7w7 <99504287+Loon-7w7@users.noreply.github.com>
2022-06-13 10:49:23 -0500

    formulario

--- FixReportSystem/Startup.cs
+++ FixReportSystem/Startup.cs
@@ -50,7 +50,7 @@ namespace FixReportSystem
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=InicioDeSesion}/{action=Index}/{id?}");
             });
         }
     }
