d52c17c7b66b6f60684082de41338a279b5838a3
Hennry Chan <dual.hennry.chan@gmail.com>
2022-06-13 17:08:28 -0500

    Se agrego edit y delete

--- FixReportSystem/Controllers/ReportController.cs
+++ FixReportSystem/Controllers/ReportController.cs
@@ -1,8 +1,10 @@
 ﻿using FixReportSystem.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace FixReportSystem.Controllers
 {
@@ -56,6 +58,89 @@ namespace FixReportSystem.Controllers
             _context.Reports.Add(report);
             _context.SaveChanges();
             return View("Index", report);
+        }
+
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var report = await _context.Reports.FindAsync(id);
+            if (report == null)
+            {
+                return NotFound();
+            }
+            ViewData["ReportId"] = new SelectList(_context.Reports, "RepordId", "ReportId", report.ReportId);
+            return View(report);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, [Bind("ReportId, Name, LastName, Building, Classroom, Furniture, Email, HeadCngineeringCourse, FurnitureOrClaim, ReportDescription, UserId, User")] Report report)
+        {
+            if (id != report.ReportId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(report);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ReportExists(report.ReportId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ReportId"] = new SelectList(_context.Reports, "ReportId", "ReportId", report.ReportId);
+            return View(report);
+        }
+
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var reports = await _context.Reports
+                .Include(c =>c.User)
+                .FirstOrDefaultAsync(m => m.ReportId == id);
+            if (reports == null)
+            {
+                return NotFound();
+            }
+
+            return View(reports);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            var report = await _context.Reports.FindAsync(id);
+            _context.Reports.Remove(report);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+
+        private bool ReportExists(string id)
+        {
+            return _context.Reports.Any(e => e.ReportId == id);
         }
     }
 }
